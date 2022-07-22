d67c96772edf0af5682654f5ea040bc6538596ec
arkadiusz971 <arkadiusz.giemza97@gmail.com>
2022-07-22 22:46:07 +0200

    change player's mark each turn

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
+const game = (function () {
+    let currentPlayer
+
+    function switchPlayer() {
+        if (currentPlayer === player1) {
+            currentPlayer = player2
+            return
+        }
+        if (currentPlayer === player2 || currentPlayer == null) {
+            currentPlayer = player1
+            return
+        }
+    }
+
+    function getCurrentPlayer() {
+        return currentPlayer
+    }
+
+    return {
+        getCurrentPlayer,
+        switchPlayer,
+    }
+})()
+
 const gameBoard = (function () {
     const _board = [
-        ['O', '', 'O'],
-        ['X', 'O', 'O'],
-        ['O', 'X', 'X'],
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
     ]
     const _boardContainer = document.querySelector('.board-container')
-    const marker = 'X'
 
     function _placeMark(e, row, column) {
-        if (!e.target.innerText) {
-            _board[row][column] = marker
+        if (!_board[row][column]) {
+            _board[row][column] = game.getCurrentPlayer().mark
+            game.switchPlayer()
             display()
         }
     }
@@ -32,7 +56,21 @@ const gameBoard = (function () {
     }
 
     return {
-        display
+        display,
     }
 })()
+
+const player = (name, mark) => {
+    return {
+        name,
+        mark,
+    }
+
+}
+
+const player1 = player("Joe", "X")
+const player2 = player("Donald", "O")
+
+game.switchPlayer()
+
 gameBoard.display()
