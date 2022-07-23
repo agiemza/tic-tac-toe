119b0d74bad808b189c69e6ec98091130be19de0
arkadiusz971 <arkadiusz.giemza97@gmail.com>
2022-07-23 10:36:30 +0200

    add win detection

--- app.js
+++ app.js
@@ -33,9 +33,38 @@ const gameBoard = (function () {
     function _placeMark(e, row, column) {
         if (!_board[row][column]) {
             _board[row][column] = game.getCurrentPlayer().mark
+
+
             game.switchPlayer()
             display()
+            _checkGameEnd()
         }
+    }
+
+    function _winDetector(field1, field2, field3) {
+        if (!!_board[field1[0]][field1[1]]
+            && _board[field1[0]][field1[1]] === _board[field2[0]][field2[1]]
+            && _board[field1[0]][field1[1]] === _board[field3[0]][field3[1]]) {
+            console.log("win")
+        }
+    }
+
+    function _checkGameEnd() {
+
+        // check rows
+        _winDetector([0, 0], [0, 1], [0, 2])
+        _winDetector([1, 0], [1, 1], [1, 2])
+        _winDetector([2, 0], [2, 1], [2, 2])
+
+        // check columns
+        _winDetector([0, 0], [1, 0], [2, 0])
+        _winDetector([0, 1], [1, 1], [2, 1])
+        _winDetector([0, 2], [1, 2], [2, 2])
+
+        //check diagonal
+        _winDetector([0, 0], [1, 1], [2, 2])
+        _winDetector([0, 2], [1, 1], [2, 0])
+
     }
 
     function _clearBoardContainer() {
